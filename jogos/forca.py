db131f86fc269b3d61e6d16ebeb6d76305d52c26
Igor Barroso Almeida <igorba867@gmail.com>
2022-12-28 17:12:14 -0300

    modify forca.py

--- jogos/forca.py
+++ jogos/forca.py
@@ -6,6 +6,11 @@
  * @Date 27/12/2022 
 '''
 
+def menu(acertos):
+    for e in acertos:
+        print(e, end=" ")
+    print("")
+
 def jogar():
     print("-----------------------------------")
     print("   Bem vindo ao jogo da forca!!   ")
@@ -14,8 +19,22 @@ def jogar():
     enforcou = False
     acertou = False
 
+    palavra_chave = "Banana"
+
+    acertos = ["_","_","_","_","_","_"]
+
+    menu(acertos=acertos)
     while((not enforcou) and (not acertou)):
-        print("Jogando...")
+
+        chute = input("Chute: ").strip() # Tira espaços do começo e final
+    
+        i = 0
+        for letra in palavra_chave:
+            if(chute.upper() == letra.upper()):
+                acertos[i] = letra
+            i += 1
+        menu(acertos=acertos)
+        
 
 if(__name__ == "__main__"):
     jogar()
