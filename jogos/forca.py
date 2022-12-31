52d25f7b153c3a40d5b876b2c9390bc26d7f834f
Igor Barroso Almeida <igorba867@gmail.com>
2022-12-30 21:22:32 -0300

    add file manipulation

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,7 +3,7 @@
  * @Author Igor Barroso Almeida
  * @Brief Jogo de forca criado
  * no curso de introdução ao python da Alura
- * @Date 27/12/2022 
+ * @Date 30/12/2022 
 '''
 
 def menu(acertos):
@@ -16,25 +16,50 @@ def jogar():
     print("   Bem vindo ao jogo da forca!!   ")
     print("-----------------------------------")
 
+    # Leitura de arquivo
+    file = open("jogos\palavras.txt", "r")
+    palavras = []
+
+    for linha in file:
+        linha = linha.strip()
+        palavras.append(linha)
+    file.close()
+
+    # Escolha aleatoria de uma palavra
+    import random
+
+    index = random.randint(0, len(palavras) -1)
+    palavra_chave = palavras[index]
+
+    # Começando o jogo
+    acertos = ["_" for letra in palavra_chave]
     enforcou = False
     acertou = False
-
-    palavra_chave = "Banana"
-
-    acertos = ["_","_","_","_","_","_"]
+    erros = 0
 
     menu(acertos=acertos)
     while((not enforcou) and (not acertou)):
 
         chute = input("Chute: ").strip() # Tira espaços do começo e final
     
-        i = 0
-        for letra in palavra_chave:
-            if(chute.upper() == letra.upper()):
-                acertos[i] = letra
-            i += 1
+        if(chute.upper() in palavra_chave.upper()):
+            i = 0
+            for letra in palavra_chave:
+                if(chute.upper() == letra.upper()):
+                    acertos[i] = letra
+                i += 1
+        else:
+            erros += 1
+            print(f"Você errou faltam {6-erros} erro(s)")
         menu(acertos=acertos)
         
+        enforcou = (erros == 6)
+        acertou = ("_" not in acertos)
+    
+    if(acertou):
+        print("Parabens você acertou!!")
+    else:
+        print("Game over, o personagem enforcou.")
 
 if(__name__ == "__main__"):
     jogar()
